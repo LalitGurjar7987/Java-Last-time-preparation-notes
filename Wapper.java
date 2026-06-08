0ef559c1864af50fe8606078fc2820d8a9e286b5
Lalit Gurjar <lalitgurjar32570@email@gmail.com>
2026-06-08 13:16:31 +0530

    short noteas

---
+++ Wapper.java
@@ -0,0 +1,25 @@
+// All Wapper Classes Are Immutable in java
+
+// Autoboxing = Autoboxing means Java automatically converts a primitive value into its wrapper class object.
+/*
+int a = 10;
+
+// Java automatically converts int to Integer
+Integer b = a;
+
+System.out.println(b); // 10
+*/
+
+// Unboxing = Unboxing means Java automatically converts a wrapper object into its primitive value.
+
+/*
+Integer a = 20;
+
+// Java automatically converts Integer to int
+int b = a;
+
+System.out.println(b); // 20
+*/
+
+
+
---
+++ access modifiers.java
@@ -0,0 +1,5 @@
+Modifier	            Same Class	Same Package	Subclass (Different Package)	Other Classes
+private	                       ✅	     ❌	                  ❌	                      ❌
+Default                        ✅	     ✅	                  ❌	                      ❌
+protected	                   ✅	     ✅	                  ✅	                      ❌
+public	                       ✅	     ✅	                  ✅	                      ✅
